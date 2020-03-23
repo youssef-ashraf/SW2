30e3e40a0ae93c847f635bb361e6025da9729ec4
katrenmikhael <61967210+katrenmikhael@users.noreply.github.com>
2020-03-23 18:51:31 +0200

    register

---
+++ laravel/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request)
+	{
+
+    	$name = $request->input('name');
+		$password = $request->input('passowrd');
+		$email = $request->input('email'); 
+		$data=db::select('select email from fli_use where email =?',[$email]);
+		if(count($data))
+		{
+			echo "you are already registered";
+		}
+		else
+		{
+				if(strlen($password)<8)
+		             {
+						echo "please enter more than 8 character in password";
+					 }
+
+				else
+					{
+						DB::insert('insert into fli_use (id,name,email,password) values(?,?,?,?)',[null,$name,$email,$password]);
+        			}		echo "welcome";
+		}			
+	}
+	public function showregester()
+	{
+		return view('register');
+	}
+	 
+}
