1aae0834f80cea188676c724dddf463a92ed5346
katrenmikhael <61967210+katrenmikhael@users.noreply.github.com>
2020-03-24 16:04:41 +0200

    user_table

--- laravel/app/Http/Controllers/RegisterController.php
+++ laravel/app/Http/Controllers/RegisterController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\DB;
 
 class RegisterController extends Controller
 {
