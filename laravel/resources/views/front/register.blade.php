61480245f511cf9713187a922f212164bf4868c7
youssef-ashraf <61710199+youssef-ashraf@users.noreply.github.com>
2020-06-01 23:06:09 +0200

    Login/Register

---
+++ laravel/resources/views/front/register.blade.php
@@ -0,0 +1,22 @@
+@extends('front.layout')
+
+@section('styles')
+
+<link rel="stylesheet" href="{{asset('front/login')}}/login.css">
+@endsection
+
+@section('content')
+<main>
+
+
+<form id="msform" action="{{route('front.register')}}" method="post" >
+ 
+{{ csrf_field() }}
+    <h2 class="fs-title">Registeration</h2>
+    <input type="text" name="name" class="input-field" placeholder="User Name" required>
+    <input type="email" name="email"class="input-field" placeholder="Email " required>
+    <input type="password" name="pass"class="input-field" placeholder="Pass" required>
+	<input type="submit" class="next action-button" value="register"> 
+  
+</main>
+@endsection
